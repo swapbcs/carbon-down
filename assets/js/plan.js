4de431a0fb3df268e91e1d48092f614994caccc5
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-11-02 21:18:53 +0100

    Retrieve the cards from local storage on plans page

---
+++ assets/js/plan.js
@@ -0,0 +1,51 @@
+const getEmissionDataFromStorage = function () {
+  // Retrieves the emission data from localStorage
+  let emissionData = localStorage.getItem("emissionData");
+  if (emissionData === null) {
+    //  we are storing the data for the first time
+    emissionData = {
+      flights: [],
+      vehicles: [],
+    };
+  } else {
+    // Convert string to json
+    emissionData = JSON.parse(emissionData);
+  }
+  return emissionData;
+};
+
+const renderFlightCarbonCard = function (emissionData) {
+  let flightsData = emissionData.flights;
+
+  for (let i = 0; i < flightsData.length; i++) {
+    // construct card
+    const card = `<div class="column">
+        <div class="card">
+        <div class="card-content">
+        <div class="content">
+            <div class="is-size-3">${flightsData[i].departure_airport} -> ${flightsData[i].destination_airport}</div>
+            <div class="is-size-5">
+            <i class="fas fa-users"></i>
+            <span class="ml-3">${flightsData[i].passengers} passengers</span>
+            </div>
+            <hr />
+            <div class="is-size-4 my-2">Carbon Emissions</div>
+            <div class="my-4">
+            <div class="is-size-5 p-2">${flightsData[i].carbon_g} grams</div>
+            <div class="is-size-5 p-2">${flightsData[i].carbon_lb} pounds</div>
+            <div class="is-size-5 p-2">${flightsData[i].carbon_kg} kilograms</div>
+            <div class="is-size-5 p-2">${flightsData[i].carbon_mt} metric tonnes</div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+  </div>`;
+
+    // append to parent
+    $("#flight-card-container").append(card);
+  }
+};
+
+let emissionData = getEmissionDataFromStorage();
+renderFlightCarbonCard(emissionData);
