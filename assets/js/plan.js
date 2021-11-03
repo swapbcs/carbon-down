cd7e5efba54bd6a47dc632e6b47cde0a13d4b34d
pillow <fard.cca@gmail.com>
2021-11-03 15:40:30 +0100

    Fix renderVehiclesCards

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -47,5 +47,31 @@ const renderFlightCarbonCard = function (emissionData) {
   }
 };
 
+const renderVehicleCarbonCard = function (emissionData) {
+  let vehiclesData = emissionData.vehicles;
+  for (let i = 0; i < vehiclesData.length; i++) {
+    const card = `<div class="card">
+    <div class="card-content">
+    <div class="content">
+    <div class="is-size-3">${vehiclesData[i].vehicle_make} - ${vehiclesData[i].vehicle_model} (${vehiclesData[i].vehicle_year})</div>
+    <hr />
+    <div class="is-size-4 my-2">Carbon Emissions</div>
+    <div class="my-4">
+    <div class="is-size-5 p-2">${vehiclesData[i].carbon_g} grams</div>
+    <div class="is-size-5 p-2">${vehiclesData[i].carbon_lb} pounds</div>
+    <div class="is-size-5 p-2">${vehiclesData[i].carbon_kg} kilograms</div>
+    <div class="is-size-5 p-2">${vehiclesData[i].carbon_mt} metric tonnes</div>
+    </div>
+    </div>
+    
+    </div>
+    </div>`;
+
+    // append to parent
+    $("#vehicle-card-container").append(card);
+  }
+};
+
 let emissionData = getEmissionDataFromStorage();
 renderFlightCarbonCard(emissionData);
+renderVehicleCarbonCard(emissionData);
